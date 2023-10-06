4ffc465e413da8548864c646ce430e8e70014cf6
nikhilchalis <nikhilchalis@gmail.com>
2023-10-06 22:17:12 +1100

    finalsied part 2 and debugged

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import pygame as pg
-from state import GameState
+from state import GameState, Move
 
+# initialise variables
 WIDTH = HEIGHT = 512
 BOARD_DIMENSION = 8
 SQUARE_SIZE = HEIGHT // BOARD_DIMENSION
 MAX_FPS = 15
 IMAGES = {}
 
+# loads images into the IMAGES dictionary
 def loadImages():
     pieces = ['wP', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bP', 'bR', 'bN', 'bB', 'bK', 'bQ']
     for piece in pieces:
         IMAGES[piece] = pg.transform.scale(pg.image.load("./images/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
 
+
+# main loop
 def main():
     pg.init()
     sc = pg.display.set_mode((WIDTH, HEIGHT))
@@ -20,23 +24,48 @@ def main():
 
     gs = GameState()
     loadImages()
+
     running = True
+    square_selected = () # no square selected - empty tuple of the form (row, col)
+    player_clicks = [] # keep track of players clicks - list of two tuples [(6, 4), (4, 4)] means start click at (6,4) and end at (4,4)
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
+            elif event.type == pg.MOUSEBUTTONDOWN:# This implementation does not allow click and drag - that could be a future addition
+                location = pg.mouse.get_pos()# returns as (x, y) tuple
+                # this works as is since there is no border and the board is the entire screen
+                # if we add a border or have the board in a larger area, we need to take care of the offsets
+                col = location[0] // SQUARE_SIZE
+                row = location[1] // SQUARE_SIZE
+
+                if square_selected == (row, col): #user clicked the same square twice - we deselect
+                    square_selected = () # deselect
+                    player_clicks = [] # clear player clicks
+                else:
+                    square_selected = (row, col)
+                    player_clicks.append(square_selected)
+
+                # was that the users second click?
+                if len(player_clicks) == 2: # after second click
+                    move = Move(player_clicks[0], player_clicks[1], gs.board)
+                    print(f"move made: {move.get_chess_notation()}")
+                    gs.make_move(move)
+                    square_selected = ()
+                    player_clicks = []
+
         
-        drawGameState(sc, gs)
+        draw_game_state(sc, gs)
         clock.tick(MAX_FPS)
         pg.display.flip()
 
 
-def drawGameState(screen, gs):
-    drawBoard(screen)
-    drawPieces(screen, gs.board)
+def draw_game_state(screen, gs):
+    draw_board(screen)
+    draw_pieces(screen, gs.board)
 
 
-def drawBoard(screen):
+def draw_board(screen):
     board_colors = [pg.Color("white"), pg.Color("gray")]
     for row in range(BOARD_DIMENSION):
         for col in range(BOARD_DIMENSION):
@@ -44,7 +73,7 @@ def drawBoard(screen):
             pg.draw.rect(screen, color, pg.Rect(col*SQUARE_SIZE, row*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
 
-def drawPieces(screen, board):
+def draw_pieces(screen, board):
     for row in range(BOARD_DIMENSION):
         for col in range(BOARD_DIMENSION):
             piece = board[row][col]
--- state.py
+++ state.py
@@ -2,6 +2,7 @@ import numpy as np
 
 class GameState():
     def __init__(self):
+        
         self.board = np.array([
                 ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
                 ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
@@ -12,6 +13,41 @@ class GameState():
                 ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
                 ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
             ])
-        self.whiteToMove = True
-        self.moveLog = []
+        self.white_to_move = True
+        self.move_log = []
+    
+
+    def make_move(self, move):
+        self.board[move.start_row][move.start_col] = "--" # starting square will be empty
+        self.board[move.end_row][move.end_col] = move.piece_moved
+        self.move_log.append(move)
+        self.white_to_move = not self.white_to_move # switch colours
         
+
+class Move():
+
+    # mapping rows and cols to rank-file values
+    ranks_to_rows = {"1" : 7, "2" : 6, "3" : 5, "4" : 4,
+                     "5" : 3, "6" : 2, "7" : 1, "8" : 0}
+    rows_to_ranks = {v: k for k, v in ranks_to_rows.items()}
+
+    files_to_cols = {"a" : 0, "b" : 1, "c" : 2, "d" : 3,
+                     "e" : 4, "f" : 5, "g" : 6, "h" : 7}
+    cols_to_files = {v: k for k, v in files_to_cols.items()}
+
+
+    def __init__(self, start_square, end_square, board):
+        self.start_row = start_square[0]
+        self.start_col = start_square[1]
+        self.end_row = end_square[0]
+        self.end_col = end_square[1]
+
+        self.piece_moved = board[self.start_row][self.start_col]
+        self.piece_captured = board[self.end_row][self.end_col]
+
+
+    def get_chess_notation(self):
+        return self.get_rank_file(self.start_row, self.start_col) + self.get_rank_file(self.end_row, self.end_col)
+
+    def get_rank_file(self, row, col):
+        return self.cols_to_files[col] + self.rows_to_ranks[row]
